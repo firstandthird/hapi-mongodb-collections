e366211a24d344a5630e617822d5e518b555135c
Eric Willis <eric@ecwillis.com>
2017-12-28 08:48:39 +0000

    Updated to hapi-17 version

--- test/index.test.js
+++ test/index.test.js
@@ -1,95 +1,112 @@
-/* globals describe, before, after, it */
 'use strict';
-const chai = require('chai');
-const assert = chai.assert;
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+const code = require('code');
+
 const Hapi = require('hapi');
 
-const launchServer = (server, port, mongoOpts, pluginOpts, done) => {
-  server.connection({ port });
-  server.register([
-    {
-      register: require('hapi-mongodb'),
+lab.experiment('hapi-mongo-collections', () => {
+  // Define the options --
+  const mongoOpts = {
+    url: 'mongodb://localhost:27017/hapimongocoll'
+  };
+  const pluginOpts = {
+    collections: ['testcollection']
+  };
+
+  let server;
+  lab.before(async() => {
+    server = new Hapi.Server({ port: 8080 });  
+
+    await server.register({
+      plugin: require('hapi-mongodb'),
       options: mongoOpts
-    },
-    {
-      register: require('../'),
+    });
+
+    await server.register({
+      plugin: require('../'),
       options: pluginOpts
-    }
-  ], (err) => {
-    if (err) {
-      console.log(err);
-    }
-    assert(err === undefined);
-    server.start((startErr) => {
-      if (startErr) {
-        return done(startErr);
-      }
-      done();
     });
   });
-};
 
-let server;
-const port = 8083;
-const mongoOpts = {
-  url: 'mongodb://localhost:27017',
-  decorate: undefined
-};
-const pluginOpts = {
-  collections: ['testcollection']
-};
-
-describe('hapi-mongodb-collections', () => {
-  before(done => {
-    server = new Hapi.Server();
-    launchServer(server, port, mongoOpts, pluginOpts, done);
+  lab.after(async() => {
+    await server.stop();
   });
 
-  after(done => {
-    server.stop(done);
-  });
-
-  it('should decorate server', done => {
-    assert(typeof server.testcollection === 'object', 'Collection exists');
-    assert(typeof server.testcollection.find === 'function', 'Has functions');
-    done();
+  lab.test('it should decorate the server', () => {
+    code.expect(server.testcollection).to.be.an.object();
+    code.expect(server.testcollection.find).to.be.a.function();
   });
 });
 
-describe('hapi-mongodb-collections decorated', () => {
-  before(done => {
-    server = new Hapi.Server();
-    mongoOpts.decorate = true;
-    launchServer(server, port, mongoOpts, pluginOpts, done);
-  });
-  after(done => {
-    server.stop(done);
+lab.experiment('hapi-mongodb-collections decorated', () => {
+  // Define the options --
+  const mongoOpts = {
+    url: 'mongodb://localhost:27017/hapicollectiondec',
+    decorate: true
+  };
+  const pluginOpts = {
+    collections: ['testcollection']
+  };
+
+  let server;
+  lab.before(async() => {
+    server = new Hapi.Server({ port: 8080 });  
+
+    await server.register({
+      plugin: require('hapi-mongodb'),
+      options: mongoOpts
+    });
+
+    await server.register({
+      plugin: require('../'),
+      options: pluginOpts
+    });
   });
 
-  it('should work', done => {
-    assert(typeof server.testcollection === 'object', 'Collection exists');
-    assert(typeof server.testcollection.find === 'function', 'Has functions');
-    done();
+  lab.after(async() => {
+    await server.stop();
+  });
+
+  lab.test('it should decorate the server as well', () => {
+    code.expect(server.testcollection).to.be.an.object();
+    code.expect(server.testcollection.find).to.be.a.function();
   });
 });
 
-describe('hapi-mongodb-collections namespace', () => {
-  before(done => {
-    server = new Hapi.Server();
-    mongoOpts.decorate = true;
-    pluginOpts.namespace = 'dbCollections';
-    pluginOpts.collections = ['stamps', 'rocks'];
+lab.experiment('hapi-mongodb-collections w/namespace', () => {
+  // Define the options --
+  const mongoOpts = {
+    url: 'mongodb://localhost:27017/hapicollectiondec',
+    decorate: true
+  };
+  const pluginOpts = {
+    collections: ['stamps', 'rocks'],
+    namespace: 'colls'
+  };
 
-    launchServer(server, port, mongoOpts, pluginOpts, done);
-  });
-  after(done => {
-    server.stop(done);
+  let server;
+  lab.before(async() => {
+    server = new Hapi.Server({ port: 8080 });  
+
+    await server.register({
+      plugin: require('hapi-mongodb'),
+      options: mongoOpts
+    });
+
+    await server.register({
+      plugin: require('../'),
+      options: pluginOpts
+    });
   });
 
-  it('should be able to specify a namespace', done => {
-    assert(typeof server.dbCollections.stamps === 'object', 'Collection exists');
-    assert(typeof server.dbCollections.rocks === 'object', 'Multiple collections can be in same namespace');
-    assert(typeof server.dbCollections.stamps.find === 'function', 'Has functions');
-    done();
+  lab.after(async() => {
+    await server.stop();
+  });
+
+  lab.test('it should decorate the server as well', () => {
+    code.expect(server.colls.stamps).to.be.an.object();
+    code.expect(server.colls.rocks).to.be.an.object();
+    code.expect(server.colls.stamps.find).to.be.a.function();
   });
 });
